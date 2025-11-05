c3ced689372e832096ef34f23987bedec838d8ad
MahmudAze <mahmudrahimli2006@gmail.com>
2025-11-05 22:08:47 +0400

    Thread, Await

---
+++ Thread, Await, Async/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Thread__Await__Async
+{
+
+    class Program
+    {
+        #region Thread
+        //static void SayHello()
+        //{
+        //    for (int i = 0; i < 5; i++)
+        //    {
+        //        Console.WriteLine("Thread-e SALAM!");
+        //        Thread.Sleep(500);
+        //    }
+        //}
+
+        //static void Main()
+        //{
+        //    Thread t = new Thread(SayHello);
+        //    t.Start(); 
+
+
+        //    for (int i = 0; i < 5; i++)
+        //    {
+        //        Console.WriteLine("Eses Thread calisir.");
+        //        Thread.Sleep(500);
+        //    }
+        //}
+        #endregion
+
+        #region async
+        //static async Task Main()
+        //{
+        //    Console.WriteLine("Is baslayir...");
+        //    await Task.Delay(2000); 
+        //    Console.WriteLine("Is Bitdi!");
+        //}
+        #endregion
+    }
+
+}
+
+
